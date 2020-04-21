ec43bc2ab99595690f063fb72ae84b2bc35109e3
Dustin Moorman <dustin.moorman@appdynamics.com>
2020-04-21 06:42:01 -0500

    revert to old sql driver

--- bin/sellM3s.sh
+++ bin/sellM3s.sh
@@ -5,6 +5,6 @@ HOST="http://dmoormandevelopmentlab-qux2vh7z.appd-sales.com:8080/Cars_Sample_App
 for ((i=1;i<=20;i++));
 do
 		echo "Call number: ${i} for DOWNSTREAM: ${DOWNSTREAM_URL}"
-        curl -X POST -H 'Content-Type: application/json' -d '{ "manufacturer": "4", "model": "M3", "name": "Alpine", "colour": "Schwarz", "year": "2003", "price": "45000", "summary": "Schwarzalpina", "description": "Schwarzalpina2" }' $HOST
+        curl -X POST -H 'Content-Type: application/json' -F "manufacturer=4" -F "model=M3" -F "name=Alpine" -F "colour=Schwarz" -F "year=2003" -F "price=45000" -F "summary=Schwarzalpina" -F "description=Schwarzalpina2" -F "x=23" -F "y=22" $HOST
         sleep 1
 done
